bcb2107f15dc31d40eecbc2013000f744f3b8aff
Taylor Bisset <taylorbisset@gmail.com>
2023-09-24 11:28:22 -0700

    Comment in basic header information.

--- CS132-VideoGameFightSim-HonorsProject/CS132-VideoGameFightSim-HonorsProject.cpp
+++ CS132-VideoGameFightSim-HonorsProject/CS132-VideoGameFightSim-HonorsProject.cpp
@@ -1,3 +1,11 @@
+/*
+ Author:        Taylor Bisset
+ Section:       A
+ Assignment:    P1
+ Description:   This project is for my AAS in Computer Science Honors coursework. 
+                Professor Lonnie Heinke is my mentor through this quarters assignments. 
+*/
+
 #include <iostream>
 
 int main()
