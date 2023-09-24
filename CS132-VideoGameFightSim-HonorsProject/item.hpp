dd273348ccab19cee1d1c21ecdc0f6b6bcf442d3
Taylor Bisset <taylorbisset@gmail.com>
2023-09-24 14:39:27 -0700

    Create Item class

---
+++ CS132-VideoGameFightSim-HonorsProject/item.hpp
@@ -0,0 +1,33 @@
+/*
+item.hpp
+Item subclass for all items in the game.
+This class is the main item class from which all items will inherit.
+*/
+#ifndef ITEM_HPP
+#define ITEM_HPP
+
+#include "entity.hpp"
+
+class Item : public Entity
+{
+public:
+	Item(const string& name, const string& description, 
+		const string& type, int value) :
+
+		Entity(name, description), 
+		type(type), value(value)
+	{
+		// Item properties
+	}
+
+	string getType() 
+		const { return type; }
+	int    getValue() 
+		const { return value; }
+
+private:
+	string type; // armor, weapon
+	int	   value;
+};
+
+#endif // !ITEM_HPP
