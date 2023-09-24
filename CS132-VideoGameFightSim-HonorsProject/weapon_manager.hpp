2029c3958e95ce76cf27122cb9d4afee02760f0e
Taylor Bisset <taylorbisset@gmail.com>
2023-09-24 16:22:56 -0700

    Added ranged weapons to Weapn Manager

--- CS132-VideoGameFightSim-HonorsProject/weapon_manager.hpp
+++ CS132-VideoGameFightSim-HonorsProject/weapon_manager.hpp
@@ -5,8 +5,12 @@ The Weapon Manager creates instances of all weapons in the game.
 
 #include "weapon.hpp"
 				// name, description, type,      value, requiredLevel, damage 
-Weapon IronLKnife("Iron Knife", "A small knife for cutting", "Weapon",		15,  1,  5);
-Weapon IronDagger("Iron Dagger", "A short blade for quick work", "Weapon",	25,  2, 10);
-Weapon IronSword("Iron Sword", "A basic sword made of iron", "Weapon",		50,  5, 25);
-Weapon IronLongsword("Iron Longsword", "A long-bladed sword", "Weapon",		75,  8, 40);
-Weapon IronClaymore("Iron Claymore", "A massive iron sword", "Weapon",	   100, 10, 55);
+Weapon IronLKnife("Iron Knife", "A small knife for cutting", "Melee",		15,  1,  5);
+Weapon IronDagger("Iron Dagger", "A short blade for quick work", "Melee",	25,  2, 10);
+Weapon IronSword("Iron Sword", "A basic sword made of iron", "Melee",		50,  5, 25);
+Weapon IronLongsword("Iron Longsword", "A long-bladed sword", "Melee",		75,  8, 40);
+Weapon IronClaymore("Iron Claymore", "A massive iron sword", "Melee",	   100, 10, 55);
+
+Weapon ShortBow("Short Bow", "A fitting bow for a small hunt", "Ranged",		15, 1, 5);
+Weapon CrossBow("Crossbow", "Great for protecting a Keep", "Ranged",			25, 2, 10);
+Weapon LongBow("Longbow", "A Longbow suitable for decimating armies", "Ranged", 50, 5, 25);
