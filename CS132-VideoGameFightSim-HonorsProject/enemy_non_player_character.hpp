2cc806a934034d0b7e472287ac89cf1523bbcb3e
Taylor Bisset <taylorbisset@gmail.com>
2023-09-24 14:25:03 -0700

    Created enemyNPC class

---
+++ CS132-VideoGameFightSim-HonorsProject/enemy_non_player_character.hpp
@@ -0,0 +1,32 @@
+/*
+enemy_non_player_character.hpp
+Non_Player_Character subclass for NPCs.
+This class is the where all the NPCs will be built from.
+*/
+#ifndef ENEMY_NON_PLAYER_CHARACTER_HPP
+#define ENEMY_NON_PLAYER_CHARACTER_HPP
+
+#include "non_player_character.hpp"
+
+class EnemyNonPlayerCharacter : public NonPlayerCharacter
+{
+public:
+    EnemyNonPlayerCharacter(const string& name, const string& description,
+        int level, int health, int attack, int defense,
+        int attitude, 
+        int loot) :
+
+        NonPlayerCharacter(name, description,
+            level, health, attack, defense, 
+            attitude),
+
+        loot(loot)
+    {
+        // Enemy NPC properties
+    }
+
+private:
+    int loot;
+};
+
+#endif // ENEMY_NON_PLAYER_CHARACTER_HPP
