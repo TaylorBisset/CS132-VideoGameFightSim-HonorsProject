2e6efdb8dc403f713f75954b02489b30fb742a7d
Taylor Bisset <taylorbisset@gmail.com>
2023-10-11 12:35:05 -0700

    Create pause_menu_state.hpp

---
+++ CS132-VideoGameFightSim-HonorsProject/pause_menu_state.hpp
@@ -0,0 +1,19 @@
+/*
+pause_menu_state.hpp
+Subclass of Game State.
+This class is the game state for the pause menu.
+*/
+#ifndef PAUSE_MENU_STATE_HPP
+#define PAUSE_MENU_STATE_HPP
+
+#include "game_state.hpp"
+
+class PauseMenuState : public GameState
+{
+public:
+    void Initialize() override;  // Initialize game state
+    void HandleInput() override; // Handle player input
+    void Update() override;      // Implement game mechanics
+};
+
+#endif // !PAUSE_MENU_STATE_HPP
