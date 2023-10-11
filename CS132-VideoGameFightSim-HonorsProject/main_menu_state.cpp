8cefd3b7a865c3edf3bc00cbd37691a3fa1fbff9
Taylor Bisset <taylorbisset@gmail.com>
2023-10-11 12:33:09 -0700

    Create main_menu_state.hpp

---
+++ CS132-VideoGameFightSim-HonorsProject/main_menu_state.cpp
@@ -0,0 +1,19 @@
+/*
+main_menu_state.hpp
+Subclass of Game State.
+This class is the game state for the main menu.
+*/
+#ifndef MAIN_MENU_STATE_HPP
+#define MAIN_MENU_STATE_HPP
+
+#include "game_state.hpp"
+
+class MainMenuState : public GameState
+{
+public:
+    void Initialize() override;  // Initialize game state
+    void HandleInput() override; // Handle player input
+    void Update() override;      // Implement game mechanics
+};
+
+#endif // !MAIN_MENU_STATE_HPP
