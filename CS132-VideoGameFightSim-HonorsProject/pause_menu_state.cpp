3bbfc40b300a9cbc3e613d1869b92bf810152146
Taylor Bisset <taylorbisset@gmail.com>
2023-10-15 16:07:40 -0700

    Create PauseMenuState::HandleInput

--- CS132-VideoGameFightSim-HonorsProject/pause_menu_state.cpp
+++ CS132-VideoGameFightSim-HonorsProject/pause_menu_state.cpp
@@ -12,3 +12,29 @@ void PauseMenuState::Initialize()
     cout << "2. Save Game\n";
     cout << "3. Quit to Main Menu\n\n";
 }
+
+void PauseMenuState::HandleInput()
+{
+    int choice;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        // Resume the game
+        // Update game state to resume play
+        break;
+    case 2:
+        // Save the game
+        // Implement game save functionality
+        break;
+    case 3:
+        // Quit to the main menu
+        // Update game state to transition to the main menu
+        break;
+    default:
+        // Handle invalid input
+        cout << "Invalid choice. Please select a valid option.\n\n";
+        break;
+    }
+}
