5d7b8574780a781d5d787eb31c32e65104dfc867
Taylor Bisset <taylorbisset@gmail.com>
2023-10-15 15:58:41 -0700

    Start work on MainMenuState::HandleInput

--- CS132-VideoGameFightSim-HonorsProject/main_menu_state.cpp
+++ CS132-VideoGameFightSim-HonorsProject/main_menu_state.cpp
@@ -12,3 +12,26 @@ void MainMenuState::Initialize()
     cout << "2. Continue\n";
     cout << "3. Quit\n\n";
 }
+
+void MainMenuState::HandleInput()
+{
+    int choice;
+    cin >> choice;
+
+    switch (choice) 
+    {
+    case 1:
+        // Start a new game
+        break;
+    case 2:
+        // Continue a saved game
+        break;
+    case 3:
+        // Quit the game
+        exit(0);
+    default:
+        // Handle invalid input
+        cout << "Invalid choice. Please select a valid option.\n\n";
+        break;
+    }
+}
