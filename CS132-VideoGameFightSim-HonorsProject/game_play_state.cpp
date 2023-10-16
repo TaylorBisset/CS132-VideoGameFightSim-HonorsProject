7b01703295e0e3b1e5560bb193e451a5b53889f1
Taylor Bisset <taylorbisset@gmail.com>
2023-10-16 08:22:51 -0700

    Set up pseudocode for game_play_state.cpp

---
+++ CS132-VideoGameFightSim-HonorsProject/game_play_state.cpp
@@ -0,0 +1,21 @@
+/*
+game_play_state.cpp
+This file implements the Gameplay mechanics
+*/
+
+#include "game_play_state.hpp"
+
+void GamePlayState::Initialize()
+{
+    // Initialize game resources
+}
+
+void GamePlayState::HandleInput()
+{
+    // Handle player input
+}
+
+void GamePlayState::Update()
+{
+    // Implement game mechanics
+}
