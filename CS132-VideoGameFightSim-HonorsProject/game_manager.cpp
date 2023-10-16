3758214238fd8dd99f451d7d2690341969fcf722
Taylor Bisset <taylorbisset@gmail.com>
2023-10-15 18:43:58 -0700

    Implement GameManager::Initialize()

--- CS132-VideoGameFightSim-HonorsProject/game_manager.cpp
+++ CS132-VideoGameFightSim-HonorsProject/game_manager.cpp
@@ -3,6 +3,7 @@ game_manager.cpp
 This file implements the Game States and the Game Loop
 */
 #include "game_manager.hpp"
+#include "main_menu_state.hpp"
 
 GameManager::GameManager()
 {
@@ -14,4 +15,10 @@ GameManager::~GameManager()
     {
         delete currentState;    // Release any allocated resources
     }
+}
+
+void GameManager::Initialize()
+{
+    currentState = new MainMenuState(); // Initialize the game manager
+    currentState->Initialize();         // set the initial state
 }
