d67a6fb237f4f27dfd0a19e0eae27cb442b8d3e5
Taylor Bisset <taylorbisset@gmail.com>
2023-10-16 10:52:34 -0700

    Updated values for armor and weapons

--- CS132-VideoGameFightSim-HonorsProject/armor_manager.hpp
+++ CS132-VideoGameFightSim-HonorsProject/armor_manager.hpp
@@ -7,19 +7,19 @@ The Armor Manager creates instances of all armors in the game.
 // name, description, type, 
 // value, requiredLevel, protection 
 Armor ClothArmor("Cloth \"Armor\"", "Simple clothes for a simple person", "Armor",
-	15,  1,  5);
+	10,  1,  5);
 Armor LeatherArmor("Leather Armor", "Armor sewn from sturdy cow leather", "Armor",
-	25,  4, 20);
+	20,  3, 10);
 Armor IronArmor("Iron Armor", "Standard issue armor for the king's men", "Armor",
-	50,  7, 45);
+	30,  7, 15);
 Armor SteelArmor("Steel Armor", "The ultimate protection for the finest warriors", "Armor", 
-	90, 10, 65);
+	40, 10, 20);
 
 Armor Buckler("Buckler", "A small wooden disk strapped to your forearm", "Shield",
-	15,  1,  5);
+	10,  2,  5);
 Armor WoodenShield("Wooden Shield", "A round shield fit for the field", "Shield",
-	35,  3, 20);
+	20,  4, 10);
 Armor IronShield("Iron Shield", "An iron shield issued by the King's men", "Shield",
-	55,  6, 50);
+	30,  8, 15);
 Armor SteelShield("Tower Shield", "A steel wall that stops any enemy onslaught", "Shield",
-	75, 10, 85);
+	40, 10, 20);
--- CS132-VideoGameFightSim-HonorsProject/weapon_manager.hpp
+++ CS132-VideoGameFightSim-HonorsProject/weapon_manager.hpp
@@ -7,19 +7,19 @@ The Weapon Manager creates instances of all weapons in the game.
 // name, description, type,
 // value, requiredLevel, damage 
 Weapon IronLKnife("Iron Knife", "A small knife for cutting", "Melee",
-	15,  1,  5);
+	10,  1,  10);
 Weapon IronDagger("Iron Dagger", "A short blade for quick work", "Melee",
-	25,  2, 10);
+	20,  3, 20);
 Weapon IronSword("Iron Sword", "A basic sword made of iron", "Melee",
-	50,  5, 25);
+	30,  5, 30);
 Weapon IronLongsword("Iron Longsword", "A long-bladed sword", "Melee",
-	75,  8, 40);
+	40,  7, 40);
 Weapon IronClaymore("Iron Claymore", "A massive iron sword", "Melee",
-	90, 10, 55);
+	50, 10, 50);
 
 Weapon ShortBow("Short Bow", "A fitting bow for a small hunt", "Ranged",
-	15, 1, 5);
+	15, 1, 15);
 Weapon CrossBow("Crossbow", "Great for protecting a Keep", "Ranged",
-	25, 2, 10);
+	30, 5, 30);
 Weapon LongBow("Longbow", "A Longbow suitable for decimating armies", "Ranged",
-	50, 5, 25);
+	45, 8, 45);
