28e8cd8dfa4eeeb4b63271befd366602cc5d6b67
Taylor Bisset <taylorbisset@gmail.com>
2023-10-29 16:05:42 -0700

    Add project header and footer to main.cpp

--- CS132-VideoGameFightSim-HonorsProject/main.cpp
+++ CS132-VideoGameFightSim-HonorsProject/main.cpp
@@ -1,3 +1,19 @@
+/*
+ Author:        Taylor Bisset
+ Section:       A
+ Assignment:    Honors Project
+ Description:   This project is for my AAS in Computer Science Honors coursework.
+				Professor Lonnie Heinke is my mentor through this quarter's assignments.
+
+				The game will be a text-based console-window fighting simulator game.
+
+Repo:           https://github.com/TaylorBisset/CS132-VideoGameFightSim-HonorsProject
+*/
+
+/*
+main.cpp
+*/
+
 #include <iostream>	
 
 using namespace std;
@@ -8,3 +24,27 @@ int main()
 
 	return 0;
 }
+
+/*
+
+Entity
+|
+|-Character
+|  |
+|  |--Player_Character
+|  |
+|  |--Non_Player_Character
+|      |
+|      |---Enemy_Non_Player_Character
+|
+|-Item
+   |
+   |--Armor
+   |   |
+   |   |---Armor_Manager
+   |
+   |--Weapon
+   |   |
+   |   |---Weapon_Manager
+
+*/
