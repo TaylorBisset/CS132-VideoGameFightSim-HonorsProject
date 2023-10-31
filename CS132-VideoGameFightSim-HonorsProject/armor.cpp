e499830cd3bdcc07a8d876726cd67b618dfb923c
Taylor Bisset <taylorbisset@gmail.com>
2023-10-30 22:29:49 -0700

    Weapons added to weapon.cpp

--- CS132-VideoGameFightSim-HonorsProject/armor.cpp
+++ CS132-VideoGameFightSim-HonorsProject/armor.cpp
@@ -1,4 +1,6 @@
 // armor.cpp
 #include "armor.hpp"
 
+// name, description, type,		value, protection
+
 Armor leatherArmor("Leather Armor", "Protective Leather Armor.", "Armor", 20, 10);
--- CS132-VideoGameFightSim-HonorsProject/weapon.cpp
+++ CS132-VideoGameFightSim-HonorsProject/weapon.cpp
@@ -1,6 +1,12 @@
 // weapon.cpp
 #include "weapon.hpp"
 
-Weapon ironSword("Iron Sword", "Sharp Iron Sword.", "Weapon", 20, 10);
+// name, description, type,		value, damage
 
-Weapon shortBow("Short Bow", "Compact Short Bow.", "Weapon", 20, 5);
+Weapon Dagger("A Short Dagger", "Short Iron Blade.", "Weapon", 10, 5);
+Weapon IronSword("An Iron Sword", "Sharp Iron Sword.", "Weapon", 20, 10);
+Weapon SteelSword("A Steel Sword", "Long Steel Sword.", "Weapon", 40, 20);
+
+Weapon Sling("Sling", "Leather Sling.", "Weapon", 10, 5);
+Weapon ShortBow("Short Bow", "Compact Short Bow.", "Weapon", 20, 10);
+Weapon LongBow("Long Bow", "Large Long Bow.", "Weapon", 40, 20);
