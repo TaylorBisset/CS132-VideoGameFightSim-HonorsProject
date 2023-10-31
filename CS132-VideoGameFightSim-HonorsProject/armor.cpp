124d31b9eaf330f17a69a9a2b2fcd5968ef41b44
Taylor Bisset <taylorbisset@gmail.com>
2023-10-30 22:32:17 -0700

    Armors added to armor.cpp

--- CS132-VideoGameFightSim-HonorsProject/armor.cpp
+++ CS132-VideoGameFightSim-HonorsProject/armor.cpp
@@ -3,4 +3,6 @@
 
 // name, description, type,		value, protection
 
-Armor leatherArmor("Leather Armor", "Protective Leather Armor.", "Armor", 20, 10);
+Armor ClothArmor("Cloth \"Armor\"", "Cloth rags.", "Armor", 10, 5);
+Armor LeatherArmor("Leather Armor", "Protective leather.", "Armor", 20, 10);
+Armor IronArmor("Iron Armor", "Iron Platemail.", "Armor", 40, 20);
